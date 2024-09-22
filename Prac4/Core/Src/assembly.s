e04e4279367f7e5b0311f81f1959230c3d419f6b
JamesGoodhead <41578387+JamesGoodhead@users.noreply.github.com>
2024-09-22 16:17:35 +0200

    added Prac 4

---
+++ Prac4/Core/Src/assembly.s
@@ -0,0 +1,54 @@
+/*
+ * assembly.s
+ *
+ */
+ 
+ @ DO NOT EDIT
+	.syntax unified
+    .text
+    .global ASM_Main
+    .thumb_func
+
+@ DO NOT EDIT
+vectors:
+	.word 0x20002000
+	.word ASM_Main + 1
+
+@ DO NOT EDIT label ASM_Main
+ASM_Main:
+
+	@ Some code is given below for you to start with
+	LDR R0, RCC_BASE  		@ Enable clock for GPIOA and B by setting bit 17 and 18 in RCC_AHBENR
+	LDR R1, [R0, #0x14]
+	LDR R2, AHBENR_GPIOAB	@ AHBENR_GPIOAB is defined under LITERALS at the end of the code
+	ORRS R1, R1, R2
+	STR R1, [R0, #0x14]
+
+	LDR R0, GPIOA_BASE		@ Enable pull-up resistors for pushbuttons
+	MOVS R1, #0b01010101
+	STR R1, [R0, #0x0C]
+	LDR R1, GPIOB_BASE  	@ Set pins connected to LEDs to outputs
+	LDR R2, MODER_OUTPUT
+	STR R2, [R1, #0]
+	MOVS R2, #0         	@ NOTE: R2 will be dedicated to holding the value on the LEDs
+
+@ TODO: Add code, labels and logic for button checks and LED patterns
+
+main_loop:
+
+
+write_leds:
+	STR R2, [R1, #0x14]
+	B main_loop
+
+@ LITERALS; DO NOT EDIT
+	.align
+RCC_BASE: 			.word 0x40021000
+AHBENR_GPIOAB: 		.word 0b1100000000000000000
+GPIOA_BASE:  		.word 0x48000000
+GPIOB_BASE:  		.word 0x48000400
+MODER_OUTPUT: 		.word 0x5555
+
+@ TODO: Add your own values for these delays
+LONG_DELAY_CNT: 	.word 0
+SHORT_DELAY_CNT: 	.word 0
